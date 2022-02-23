603b117f4a5bcbdcc8357bfcfa52f24229920a1c
BahaMaksudov <bmaksudov@gmail.com>
2022-02-22 20:05:09 -0500

    Abstraction

---
+++ Spring-04-Abstruction/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,25 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    //create constructor - has a relationship
+    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment (Comment comment) {
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
+
+}
